4f6da6f6c4506cfb1eb951cb6811c6e39f3cd35d
sanjid <sanjidtalukder02@gmail.com>
2025-02-26 22:40:16 +0600

    This is commite for add money section

---
+++ addMoney.js
@@ -0,0 +1,17 @@
+document.getElementById("add-money").addEventListener("click",function(event){
+event.preventDefault();
+
+const ammount = document.getElementById("Amount").value;
+const convertedAmmount = parseFloat(ammount)
+const pin = document.getElementById("pin").value;
+const convertedPin = parseInt(pin)
+const mainBlance = document.getElementById("main-blance").innerText;
+const convertedMainBlances = parseFloat(mainBlance)
+if(convertedPin ===1234){
+    const sum = convertedMainBlances + convertedAmmount;
+    document.getElementById("main-blance").innerText = sum;
+}else{
+    console.log("pin thik na ")
+}
+console.log(ammount)
+})
